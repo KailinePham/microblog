99c128557cd2ecb9515bd5ecac36906571ed0bc3
Kailine Pham <kailinepham@gmail.com>
2024-03-06 05:25:47 -0800

    updated base.html and created login view function for sign-in page compatibility

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,11 @@
+'''
+contains view functions that are mapped to their respective URLs;
+passes data to the template for rendering 
+'''
+
 from flask import render_template
 from app import app 
+from app.forms import LoginForm
 
 # decorators: modifies the function that it follows 
 @app.route('/')     
@@ -7,7 +13,7 @@ from app import app
 
 def index(): 
     '''
-    a view function that returns a greeting as a string 
+    a view function mapped to /index url to help render the posts 
     '''
     user = {'username': 'Miguel'}
     posts = [
@@ -15,3 +21,11 @@ def index():
         {'author': {'username': 'Susan'}, 'body': 'The Avengers movie was so cool!'}
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login')
+def login():
+    '''
+    view function mapped to /login URL to help render the sign-in page
+    '''
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
