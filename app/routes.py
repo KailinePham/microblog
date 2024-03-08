c80a8a00a65a821ca3c5a2c96cee4cc0013372ea
Kailine Pham <kailinepham@gmail.com>
2024-03-07 19:45:03 -0800

    added login status msg to view function and base template

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,7 @@ contains view functions that are mapped to their respective URLs;
 passes data to the template for rendering 
 '''
 
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app 
 from app.forms import LoginForm
 
@@ -22,10 +22,19 @@ def index():
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST']) 
+# methods arg tells flask that this view func accepts GET and POST requests 
+# default tells flask that the view function only accepts GET requests 
+#   POST requests --> used when the browser submits form data to the server 
 def login():
     '''
     view function mapped to /login URL to help render the sign-in page
     '''
     form = LoginForm()
+    # returns False when the browser sends a POST request (user hit submit)
+    if form.validate_on_submit(): # does all the form processing work
+        # stores msg to be shown later when rendered 
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remeber_me.data)) 
+        return redirect('/index') # navigate to index page 
     return render_template('login.html', title='Sign In', form=form)
